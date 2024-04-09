01492f421161f8767d3842bafcf8b658277d85b0
CARRERE Régis <regis.carrere@epitech.eu>
2024-04-09 14:55:20 +0200

    [ADDING] folders for a better organization

---
+++ README.md
@@ -0,0 +1,25 @@
+# My_Hunter project
+
+## Done by Régis CARRERE
+
+### Overview
+
+This project is a first year project of Epitech, :desktop_computer:<br>
+And its goal is to recreate a duck hunt game in CSFML in 3 weeks.<br>
+
+### Features
+
+* You have a menu from which you can start the game by pressing the `start button`.
+* You can see a beautiful, cute little duck running across your screen.
+
+### Running this project locally
+
+* Clone this repository loclly
+* Run `make` in your terminal
+* Run `./my_hunter`
+
+Then enjoy.<br>
+
+The list of the tools we used to do this project :hammer_and_wrench:<br>
+
+[![Main tools](https://skillicons.dev/icons?i=c,vscode,github,md&perline=9)](https://github.com/tandpfun/skill-icons)
---
+++ src/main.c
@@ -0,0 +1,31 @@
+/*
+** EPITECH PROJECT, 2023
+** main.c
+** File description:
+** main
+*/
+#include <unistd.h>
+#include "../include/my.h"
+
+int main(int ac, char **av)
+{
+    char *description;
+    sfVideoMode video_mode = {1920, 1080, 32};
+    sfRenderWindow* window;
+    sfEvent event;
+
+    if (ac == 2) {
+        description = "My_Hunter is an Epitech project,\n"
+                            "which consists "
+                            "of the creation of a game similar "
+                            "to duck hunt.\n";
+        if (av[1][0] == '-' && av[1][1] == 'h') {
+            my_putstr(description);
+            return 0;
+        } else {
+            return 0;
+        }
+    }
+    game_loop(window, video_mode, event);
+    return 0;
+}
